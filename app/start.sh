2acc77e6771ae1e05f42175b3cd9302a49066e72
Jack Zampolin <jack.zampolin@gmail.com>
2017-06-06 14:15:11 -0700

    Make changes"

--- app/start.sh
+++ app/start.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# Make and chown influxdb data directories
+# Make and chown influxdb data directory
 mkdir /data/influxdb
 chown influxdb:influxdb /data/influxdb
+
+# Make and chown kapacitor data directory
+mkdir /data/kapacitor
+chown kapacitor:kapacitor /data/kapacitor
+
+# Make and chown chronograf data directory
+mkdir /data/chronograf
+chown chronograf:chronograf /data/chronograf
 
 # Run the application with supervisor
 supervisord -n -c /usr/local/etc/supervisord.conf
